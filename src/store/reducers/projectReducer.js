62dfb5dc217c3e5c8b4d19498268b664e53ae4b5
syahirah <syahirah.suradi@gmail.com>
2020-03-02 00:15:41 +0800

    connected the dashboard state to the projectList and projectSummary

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+    projects: [
+        {id: "1", title: "hi i'm a turtle", content: "hey there"},
+        {id: "2", title: "whaddup", content: "look at my egg"},
+        {id: "3", title: "let's make pancakes", content: "oh i forgot eggs"}
+    ]
 }
 
 const projectReducer = (state = initState, action) => {
